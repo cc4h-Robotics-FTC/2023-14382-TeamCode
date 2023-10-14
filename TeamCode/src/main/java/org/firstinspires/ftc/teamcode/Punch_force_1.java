f4b92ea1206b226d89ee8b7804a9073ffee388bc
cc4h <cc4h@gmail.com>
2023-10-14 12:10:29 -0400

    First try at grabber code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Punch_force_1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Punch_force_1.java
@@ -4,17 +4,21 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 
 @TeleOp(group = "drive")
 public class Punch_force_1 extends LinearOpMode {
+
+    Servo servo;
     @Override
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
         drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        servo = hardwareMap.get(Servo.class, "Grabber");
 
         waitForStart();
 
@@ -28,6 +32,13 @@ public class Punch_force_1 extends LinearOpMode {
             );
 
             drive.update();
+
+            if (gamepad2.a) {
+                servo.setPosition(0);
+            }
+            if (gamepad2.b) {
+                servo.setPosition(50);
+            }
 
             Pose2d poseEstimate = drive.getPoseEstimate();
             telemetry.addData("x", poseEstimate.getX());
